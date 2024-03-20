bbc1b924d30bb6c5f8b26fb90d2d7d77e382a885
Abhishek MV <abmath374@gmail.com>
2024-03-20 17:40:44 +0530

    Tab Work Begin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 
 import state from "../store";
+import { CustomButton } from "../components";
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -36,6 +37,13 @@ const Home = () => {
                 Create your unique and exclusive shirt with our brand-new 3d customization tool. 
                 <strong>{' '}Unleash your imagination</strong>{' '}and define your own style.
               </p>
+
+              <CustomButton 
+                type='filled'
+                title="Customize It"
+                handleClick={()=> state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
           
